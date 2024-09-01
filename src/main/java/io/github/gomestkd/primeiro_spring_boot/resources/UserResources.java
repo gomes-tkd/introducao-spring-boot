44f0429a2810f331676121a38165761956d662c5
gomes-tkd <jgomestkd@gmail.com>
2024-09-01 15:43:31 -0300

    User insert

--- src/main/java/io/github/gomestkd/primeiro_spring_boot/resources/UserResources.java
+++ src/main/java/io/github/gomestkd/primeiro_spring_boot/resources/UserResources.java
@@ -4,11 +4,10 @@ import io.github.gomestkd.primeiro_spring_boot.entities.User;
 import io.github.gomestkd.primeiro_spring_boot.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -29,6 +28,16 @@ public class UserResources {
     public ResponseEntity<User> findById(@PathVariable("id") Long id) {
         User user = service.findById(id);
         return ResponseEntity.ok().body(user);
+    }
+
+    @PostMapping
+    public ResponseEntity<User> insert(@RequestBody User obj) {
+        obj = service.insert(obj);
+        URI uri = ServletUriComponentsBuilder
+                .fromCurrentRequest().path("/${id}")
+                .buildAndExpand(obj.getId())
+                .toUri();
+        return ResponseEntity.created(uri).body(obj);
     }
 
 }
