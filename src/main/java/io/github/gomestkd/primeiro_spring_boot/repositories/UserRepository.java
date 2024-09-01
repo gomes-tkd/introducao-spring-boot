e977d235be123787f904a8558d88a886f36cb92c
gomes-tkd <jgomestkd@gmail.com>
2024-08-31 23:31:33 -0300

    Service layer, component registration

--- src/main/java/io/github/gomestkd/primeiro_spring_boot/repositories/UserRepository.java
+++ src/main/java/io/github/gomestkd/primeiro_spring_boot/repositories/UserRepository.java
@@ -2,7 +2,9 @@ package io.github.gomestkd.primeiro_spring_boot.repositories;
 
 import io.github.gomestkd.primeiro_spring_boot.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
 }
--- src/main/java/io/github/gomestkd/primeiro_spring_boot/resources/UserResources.java
+++ src/main/java/io/github/gomestkd/primeiro_spring_boot/resources/UserResources.java
@@ -1,22 +1,34 @@
 package io.github.gomestkd.primeiro_spring_boot.resources;
 
 import io.github.gomestkd.primeiro_spring_boot.entities.User;
+import io.github.gomestkd.primeiro_spring_boot.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
-@RequestMapping(value = "/users")
+@RequestMapping (value = "/users")
 public class UserResources {
 
-    @GetMapping
-    public ResponseEntity<User> findAll() {
-        User user = new User(
-                1L, "Gomes", "99999999999",
-                "gomes@gmail.com", "123456"
-        );
+    @Autowired
+    private UserService service;
 
+    @GetMapping
+    public ResponseEntity<List<User>> findAll() {
+        List<User> list = service.findAll();
+
+        return ResponseEntity.ok().body(list);
+    }
+
+    @GetMapping (value = "/{id}")
+    public ResponseEntity<User> findById(@PathVariable("id") Long id) {
+        User user = service.findById(id);
         return ResponseEntity.ok().body(user);
     }
+
 }
