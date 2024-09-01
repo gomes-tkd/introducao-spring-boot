69cbe23df3ea4665fefeb05ae7601b5420659002
gomes-tkd <jgomestkd@gmail.com>
2024-09-01 16:36:49 -0300

    Exception handling - findById

--- src/main/java/io/github/gomestkd/primeiro_spring_boot/services/UserService.java
+++ src/main/java/io/github/gomestkd/primeiro_spring_boot/services/UserService.java
@@ -2,6 +2,7 @@ package io.github.gomestkd.primeiro_spring_boot.services;
 
 import io.github.gomestkd.primeiro_spring_boot.entities.User;
 import io.github.gomestkd.primeiro_spring_boot.repositories.UserRepository;
+import io.github.gomestkd.primeiro_spring_boot.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -20,7 +21,7 @@ public class UserService {
 
     public User findById(Long id) {
         Optional<User> obj = repository.findById(id);
-        return obj.get();
+        return obj.orElseThrow(() -> new ResourceNotFoundException(id));
     }
 
     public User insert(User obj) {
