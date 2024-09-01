d0b5658915d9767312b38add16f7af396d3954c2
gomes-tkd <jgomestkd@gmail.com>
2024-08-31 22:05:16 -0300

    First commit: initial files

---
+++ src/main/java/io/github/gomestkd/primeiro_spring_boot/PrimeiroSpringBootApplication.java
@@ -0,0 +1,13 @@
+package io.github.gomestkd.primeiro_spring_boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrimeiroSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PrimeiroSpringBootApplication.class, args);
+    }
+
+}
